4d2da75be70549c76aeb35bd1904930576b230d2
Xarth <xarthleo@gmail.com>
2024-03-23 17:51:54 +0800

    更新 translation.js

--- translation.js
+++ translation.js
@@ -1,25 +1,8 @@
-String.prototype.reverse = function () { return this.split("").reverse().join("") }
-
-function translateText(text) {
-    const pnyn = PinyinHelper.convertToPinyinString(text, "", PinyinFormat.WITHOUT_TONE)
-    const textline = pnyn.split("\n").map(function (line, id) {
-        return line.reverse().split(/(?<=[aeiou])(?![aeioun]|ng)/).map(function (item) {
-            const regex = [
-                [/hc/i, "ch"],
-                [/hs/i, "sh"],
-                [/hz/i, "zh"],
-                [/gn/i, "ng"],
-            ]
-            return regex.reduce(function (total, token) {
-                return total.replace(...token)
-            }, item)
-        }).join(' ')
-    })
-    return textline.join("<br/>")
-}
-
-function translate() {
+function repeatInput() {
     const inputText = document.getElementById('inputText').value.trim();
-    const translatedText = translateText(inputText);
-    document.getElementById('translationOutput').innerHTML = translatedText;
+    document.getElementById('translationOutput').innerText = inputText;
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('inputText').addEventListener('input', repeatInput);
+});
