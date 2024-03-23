8df7861cb4e8bf28c33a5e2e2c5d10c33888e378
Xarth <xarthleo@gmail.com>
2024-03-23 18:07:06 +0800

    更新 translation.js

--- translation.js
+++ translation.js
@@ -1,8 +1,4 @@
-function repeatInput() {
-    const inputText = document.getElementById('inputText').value.trim();
-    document.getElementById('translationOutput').innerText = inputText;
+function translate() {
+    const inputTextValue = document.getElementById('inputText').value.trim();
+    document.getElementById('translationOutput').innerText = inputTextValue;
 }
-
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('inputText').addEventListener('input', repeatInput);
-});
